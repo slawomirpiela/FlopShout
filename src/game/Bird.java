3b1218e01670f24f9798362eb4a988f9a2474a34
TheBear01 <neddmatthew@gmail.com>
2017-04-23 13:54:48 +0100

    Bird Class Implemented

--- src/game/Bird.java
+++ src/game/Bird.java
@@ -1,20 +1,57 @@
 package game;
 
-import processing.core.PApplet;
-import processing.core.PFont;
+import ddf.minim.AudioInput;
+import ddf.minim.Minim;
+import processing.core.*;
 
 public class Bird {
 	
 	PApplet parent;
+	PImage img;
+	Minim minum;
+	AudioInput in;
+	
+	float xpos = 200;
+	float ypos = 200;
+	float gravity = (float) 0.05;
 	
 	Bird(PApplet p) {
 		parent = p;
-	}
-	
-	void draw(){
-		
+		minum = new Minim(p);
+		in = minum.getLineIn(Minim.MONO, parent.width, 44100, 16);
 	}
 
-	
+	public void display(){
+		img = parent.loadImage("potfinal.png");
+		img.resize(35,35);
+		if(FlopShout.stage == 1){
+			parent.background(255);
+			parent.stroke(255);
+			for(int i = 0; i < in.bufferSize() - 1; i++)
+			{
+				if (parent.frameCount % 60 == 0){
+					}
+					if(in.left.get(i)*parent.height/2 > 0 && ypos > 0){
+						parent.background(255);
+						ypos = ypos - in.left.get(i)/2;
+						ypos += gravity;
+						parent.image(img, xpos, ypos);
+						if(ypos < 0)
+						{
+							ypos = ypos + 2;
+						}
+						if(ypos > 800){
+							
+							parent.background(255);
+							parent.textAlign(parent.CENTER);
+							//textFont(font);
+							parent.fill(0);
+							parent.text("Game Over", parent.width/2, parent.height/2);
+						}
+						parent.println(in.left.get(i));
+					}
+			}
+		}
+	}
 }
 
--- src/game/FlopShout.java
+++ src/game/FlopShout.java
@@ -1,30 +1,20 @@
 package game;
 
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-
 import ddf.minim.AudioInput;
 import ddf.minim.Minim;
 import processing.core.*;
 
 public class FlopShout extends PApplet {
 
-int v_value = 0;
-//MATAS IS A FAG
-Minim minum;
-AudioInput in;
-
-PImage img;
-
-PImage col;
+//PImage col;
 Menu menu;
-float xpos = 200;
-float ypos = 200;
-float b_height = 0;
+Bird bird1;
+
+//float b_height = 0;
 public static int stage = 0;
 public static int difficulty = 0;
 float x1,y1,x2,y2;
-float gravity = (float) 0.05;
+
 float border = 50;
 	
 public static void main(String[] args)
@@ -35,13 +25,10 @@ public static void main(String[] args)
 public void setup()
 	{
 		smooth();
-		minum = new Minim(this);
+		bird1 = new Bird(this);
 		menu = new Menu(this);
-		in = minum.getLineIn(Minim.MONO, width, 44100, 16);
-		
-		img = loadImage("potfinal.png");
-		img.resize(35,35);
-		col = loadImage("col.png");
+
+		//col = loadImage("col.png");
 	}
 	
 	public void settings()
@@ -51,38 +38,7 @@ public void setup()
 	
 	public void draw()
 	{
-		if(stage == 1){
-			background(255);
-			stroke(255);
-			for(int i = 0; i < in.bufferSize() - 1; i++)
-			{
-				if (frameCount % 60 == 0){
-					}
-					if(in.left.get(i)*height/2 > 0 && ypos > 0){
-						background(255);
-						ypos = ypos - in.left.get(i)/2;
-						ypos += gravity;
-						image(img, xpos, ypos);
-						if(ypos < 0)
-						{
-							ypos = ypos + 2;
-							//println(((in.left.get(i)*50w) + (in.right.get(i)*50) / 2));
-						}
-						if(ypos > 800){
-							
-							background(255);
-							textAlign(CENTER);
-							//textFont(font);
-							fill(0);
-							text("Game Over", width/2, height/2);
-						}
-					//println(in.mix.get(i));
-					//println(in.left.get(i));
-					}
-					println(in.left.get(i));
-					
-			}
-		}
+		bird1.display();
 		menu.draw();
 		//println(stage);
 	}
