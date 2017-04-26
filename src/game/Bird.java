562bda67b111aa6713236ff3169f373ed7aa258c
slawomirpiela <slawomir.piela@outlook.com>
2017-04-26 03:23:26 +0100

    after a loot of changes

--- src/game/Bird.java
+++ src/game/Bird.java
@@ -22,34 +22,12 @@ public class Bird {
 
 	public void display(){
 		heroImg = parent.loadImage("potfinal.png");
-<<<<<<< HEAD
 			b_xpos = parent.width/2;
 			parent.image(heroImg, b_xpos, b_ypos);
 			FlopShout.velocity += 1;
 			b_ypos += FlopShout.velocity;
 			//velocity += 0.5;
 			//ypos += velocity;
-=======
-		//
-			parent.image(heroImg, xpos, ypos);
-			velocity += 0.5;
-			ypos += velocity;
-			for(int i = 0; i < in.bufferSize() - 1; i++){
-				if(in.left.get(i)*parent.height/2 > 10
-						){
-					velocity =- 5;
-					//velocity = -in.left.get(i)/2;
-				//	parent.println(in.left.get(i)*parent.height/2);
-				}
-//				if(ypos < 0){
-//					ypos = ypos + 2;
-//					
-//				}
-			}
-			//if(ypos > 800){
-			//	FlopShout.stage = 4;
-			//}	
->>>>>>> d97207a057ec3a2e35ce263ad9ed2831ac01a029
 		}
 	//public void move(){
 	//	for(int i = 0; i < in.bufferSize() - 1; i++){
--- src/game/Column.java
+++ src/game/Column.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package game;
 
 import processing.core.*;
@@ -36,45 +35,3 @@ public void display(){
 		
 	}
 }
-=======
-//package game;
-//
-//import processing.core.*;
-//
-//public class Column {
-//	
-//	PApplet parent;
-//	PImage col;
-//	PImage col2;
-//
-//	PVector column;
-//	float z,c, butt;
-//	
-//	Column(PApplet p, PImage img, float x, float y, boolean rotated, boolean hittable) {
-//		column = new PVector(x,y);
-//		col = parent.loadImage("col.png");
-//		col2 = parent.loadImage("col2.png");
-//
-//		parent = p;
-//		this.column.x = x;
-//		this.column.y = y;
-//		//this.img = img;
-//		this.c = c;
-//		this.butt = butt;
-//		//this.c = c;
-//	}
-//
-//public void display(){
-//	parent.image(col, column.x, column.y);
-//}
-//
-//public void move(){
-//	column.x = column.x - parent.second()/10;
-//	c = column.y + 300; //y value of the start of the column
-//	butt = c + 300; //y value of the end of the column
-//}
-//		
-//}
-
-
->>>>>>> d97207a057ec3a2e35ce263ad9ed2831ac01a029
